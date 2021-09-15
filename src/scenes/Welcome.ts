61c9e702c565b55b86d3ce464326b374c6c71b1e
Dhardesh-crypto <dhardesh@gmail.com>
2021-09-15 21:36:31 +0200

    Powered by Moralis

--- src/scenes/Welcome.ts
+++ src/scenes/Welcome.ts
@@ -5,9 +5,11 @@ const BUTTON_NORMAL = 'buttonNormal';
 const BUTTON_HOVER = 'buttonHover';
 const BUTTON_CLICKED = 'buttonClicked';
 const BUTTON_LOCKED = 'buttonLocked';
+const MORALIS_POWERED = 'PoweredByMoralis';
 
 export default class Scene1 extends Phaser.Scene {
     private background! : Phaser.GameObjects.Image;
+    private poweredByMoralis! : Phaser.GameObjects.Image;
     private buttonPlay! : Phaser.GameObjects.Container;
     private buttonLoadNFT! : Phaser.GameObjects.Container;
     private buttonRules! : Phaser.GameObjects.Container;
@@ -43,11 +45,15 @@ export default class Scene1 extends Phaser.Scene {
         this.load.image(BUTTON_HOVER, 'assets/Button-hover.png');
         this.load.image(BUTTON_CLICKED, 'assets/Button-click.png');
         this.load.image(BUTTON_LOCKED, 'assets/Button-locked.png');
+        this.load.image(MORALIS_POWERED, 'assets/Powered-by-Moralis-Badge-Green.png');
     }
   
     create() {
       this.sound.stopAll(); // Stops all sounds from previous screens
       this.background = this.add.image(633, 800, SPLASHSCREEN_KEY).setScale(0.625).setOrigin(0,1);
+      
+      this.poweredByMoralis = this.add.image(510, 750, MORALIS_POWERED).setScale(0.5);
+
       const style = { fontSize: '32px', fill: '#fff' }
       this.welcomeText = this.add.text(50, 300, 'You are a broke knight.\n\rNo sword to defend yourself.\n\rGo collect some wealth.\n\rAnd restore your dignity.\n\rMaybe increase your might\n\rwith one or more NFTs?', style);
 
